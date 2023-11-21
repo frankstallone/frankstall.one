3f8a6388f84c7a4dede49553b16ce5b4eb1b4f0f
Frank Stallone <frank@designzen.co>
2023-11-21 16:12:02 -0500

    config: ⚙<fe0f> Updating to Astro 3.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 import purgecss from 'astro-purgecss';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
@@ -7,12 +6,5 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frankstall.one',
-  integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    purgecss(),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [purgecss(), mdx(), sitemap()],
 });
