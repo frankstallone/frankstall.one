8edf19f36b490d90959dbe088761151388c743a6
Frank Stallone <frank@designzen.co>
2023-12-09 07:27:40 -0500

    feat: ✨ Adding GA and Hotjar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 import purgecss from 'astro-purgecss';
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frankstall.one',
-  integrations: [purgecss(), sitemap()],
+  integrations: [
+    purgecss(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
