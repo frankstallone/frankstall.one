2fa0f19863d7af27c0843820526ed026761bab21
Frank Stallone <frank@designzen.co>
2024-03-21 09:32:57 -0400

    wip: 🚧 Attempting to move the POST out to an API end

---
+++ src/api/like-post/[slug].ts
@@ -0,0 +1,20 @@
+import type { APIRoute } from 'astro';
+import { db, Like } from 'astro:db';
+export const prerender = false;
+
+export const POST: APIRoute = async ({ params, request, clientAddress }) => {
+  // get slug of liked item assuming a request like `/api/like-post/[slug].ts`
+  const { slug } = params;
+  const userAgent = request.headers.get('user-agent');
+  // hash the UA, IP address, and liked post slug to create a unique ID
+  const id = await digest(userAgent + clientAddress + slug);
+  // insert the like and ignore it if it was already set
+  await db.insert(Like).values({ id, slug }).onConflictDoNothing();
+};
+
+/** Get a SHA-256 hash for a string. */
+async function digest(message: string) {
+  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8); // hash the message
+  return Buffer.from(hashBuffer).toString('base64');
+}
