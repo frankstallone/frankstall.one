753661eda9406744fc433cfdd19cb5d28d18d372
Frank Stallone <iamfrankstallone@gmail.com>
2024-03-24 06:43:55 -0400

    Adding Like buttons to posts (#10)
    * wip: 🚧 Attempting to add a simple Astro DB Like button
    
    * wip: 🚧 Attempting to move the POST out to an API end
    
    * wip: 🚧 Fixed TS errors
    
    * wip: 🚧 Moved the API to the correct folder
    
    * wip: 🚧 Able to add or remove like based on look up
    
    * feat: ✨ Still WIP but locally the Like button works
    
    * config: ⚙<fe0f> Setting --remote flag on build for Netlify
    
    * dep: ⬆️ Updating dependencies
    
    * config: ⚙<fe0f> Setting --remote flag on build for Netlify, in netlify.toml
    
    * bug: 🐛 Fixed build TS issues
    
    * bug: 🐛 Console log removedd
    
    * a11y: ♿️ Like button announces current state correctly
    
    * refactor: ♻️  Updating logo to SVG
    
    * a11y: ♿️ Like button refactored for better accessibility
    
    * ui: 💄 Refactoring look of Like button
    
    * refactor: ♻️  Adding proper members to Likes class, TS
    
    ---------
    
    Co-authored-by: Frank Stallone <frank@designzen.co>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
+import netlify from '@astrojs/netlify';
+import db from '@astrojs/db';
+
+export const prerender = false;
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: netlify(),
   site: 'https://frankstall.one',
   integrations: [
     tailwind({
@@ -16,5 +22,6 @@ export default defineConfig({
         forward: ['dataLayer.push'],
       },
     }),
+    db(),
   ],
 });
