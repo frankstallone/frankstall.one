25e335d32cb4d393cdb19219191b68e9a905d7d0
Frank Stallone <iamfrankstallone@gmail.com>
2024-09-06 14:01:03 -0400

    Feature phone input (#14)
    * remove: 🚮 Unused imports
    
    * refactor: ♻️  Cleaning up code
    
    * ui: 💄 Small change to spacing
    
    * wip: 🚧 Adding PhoneInput component and children components
    
    * wip: 🚧 Cleaning up the PhoneInput a bit
    
    * feat: ✨ Adding dynamic changing of the mask based on CountrySelector onChange
    
    * feat: ✨ Adding PhoneInput final touches
    
    ---------
    
    Co-authored-by: Frank Stallone <frank@designzen.co>

---
+++ src/components/CountrySelector.tsx
@@ -0,0 +1,80 @@
+import { useState, createElement } from 'react';
+import Select, { components } from 'react-select';
+import { Ukraine } from '../flags/ukraine';
+import { UnitedArabEmirates } from '../flags/unitedarabemirates';
+import { UnitedKingdom } from '../flags/unitedkingdom';
+import { UnitedStates } from '../flags/unitedstates';
+import { Chevron } from '../svg/chevron';
+
+interface CountrySelectorProps {
+  updateTemplate: (value: string) => void;
+}
+
+const options = [
+  { value: 'UA', label: 'Ukraine', icon: Ukraine },
+  {
+    value: 'AE',
+    label: 'United Arab Emirates',
+    icon: UnitedArabEmirates,
+  },
+  { value: 'GB', label: 'United Kingdom', icon: UnitedKingdom },
+  { value: 'US', label: 'United States', icon: UnitedStates },
+];
+
+/**
+ * CountrySelector component
+ * A customized <select> style component for international phone numbers
+ * @returns A country selector component
+ */
+
+const CountrySelector: React.FC<CountrySelectorProps> = ({
+  updateTemplate,
+}) => {
+  const [country, setCountry] = useState(options[3]);
+
+  const handleSetCountry = (e: any) => {
+    setCountry(e);
+    updateTemplate(e.value);
+  };
+
+  // This does not work: https://github.com/JedWatson/react-select/issues/3739
+  const DownChevron = (props: any) => (
+    <components.DownChevron {...props}>
+      {createElement(Chevron)}
+    </components.DownChevron>
+  );
+
+  const SingleValue = (props: any) => (
+    <components.SingleValue {...props}>
+      {createElement(country.icon)}
+    </components.SingleValue>
+  );
+
+  const Option = (props: any) => (
+    <components.Option {...props}>
+      {createElement(props.data.icon)} {props.data.label}
+    </components.Option>
+  );
+
+  return (
+    <Select
+      unstyled
+      defaultValue={country}
+      options={options}
+      onChange={handleSetCountry}
+      className="country-selector"
+      classNamePrefix="country-selector"
+      classNames={{
+        control: (state) =>
+          state.isFocused ? 'country-selector__focused' : '',
+      }}
+      isSearchable={false}
+      components={{
+        Option,
+        DownChevron,
+        SingleValue,
+      }}
+    />
+  );
+};
+export default CountrySelector;
---
+++ src/components/PhoneInput.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { templateParser, templateFormatter, parseDigit } from 'input-format';
+import ReactInput from 'input-format/react';
+import CountrySelector from './CountrySelector';
+import flags from '../flags/index.json';
+
+/**
+ * PhoneInput component
+ * A controlled input component with input masking for phone numbers
+ * @returns A phone input component
+ */
+
+export default function PhoneInput() {
+  // Phone input value
+  const [value, setValue] = useState('');
+  // US phone number template
+  const [template, setTemplate] = useState('(xxx) xxx-xxxx');
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement> | string | undefined,
+  ) => {
+    setValue(typeof event === 'string' ? event : event?.target.value || '');
+  };
+
+  function updateTemplate(iso: string) {
+    const countryToUpdate = flags.find((country: any) => country.iso === iso);
+    if (!countryToUpdate) return template;
+    if (Array.isArray(countryToUpdate.mask)) {
+      setTemplate(countryToUpdate.mask[0]);
+      setValue('');
+    } else {
+      setTemplate(countryToUpdate.mask);
+      setValue('');
+    }
+  }
+
+  const parse = templateParser(template, parseDigit);
+  const format = templateFormatter(template);
+
+  return (
+    <div className="phone-input-container">
+      <CountrySelector updateTemplate={updateTemplate} />
+      <ReactInput
+        className="phone-input"
+        value={value}
+        onChange={handleChange}
+        placeholder="Enter phone number"
+        parse={parse}
+        format={format}
+      />
+    </div>
+  );
+}
