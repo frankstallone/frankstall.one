1b52df19a5d5625f1eb895e67f699b0c342b586a
Frank Stallone <fstallone@strongtie.com>
2024-11-25 12:20:48 -0500

    remove: 🚮 Removing Astro db config and seed file due to build failure

--- db/seed.ts
+++
@@ -1,9 +0,0 @@
-import { db, Like } from 'astro:db';
-
-// https://astro.build/db/seed
-export default async function seed() {
-  await db.insert(Like).values([
-    { id: '1', slug: 'a-beginners-guide-to-digital-accessibility' },
-    { id: '2', slug: 'a-beginners-guide-to-digital-accessibility' },
-  ]);
-}
