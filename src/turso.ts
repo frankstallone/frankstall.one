c46d4d00461bf4f313efd7df474f3605166ce348
Frank Stallone <iamfrankstallone@gmail.com>
2025-06-05 12:24:03 -0400

    chore: update Astro dependencies

--- src/turso.ts
+++ src/turso.ts
@@ -1,9 +1,12 @@
 import { createClient } from '@libsql/client/web';
 import { drizzle } from 'drizzle-orm/libsql';
 
-export const turso = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL!,
-  authToken: import.meta.env.TURSO_AUTH_TOKEN,
-});
+let turso: ReturnType<typeof createClient> | undefined;
+if (import.meta.env.TURSO_DATABASE_URL) {
+  turso = createClient({
+    url: import.meta.env.TURSO_DATABASE_URL,
+    authToken: import.meta.env.TURSO_AUTH_TOKEN,
+  });
+}
 
-export const db = drizzle(turso);
+export const db = turso ? drizzle(turso) : undefined as any;
