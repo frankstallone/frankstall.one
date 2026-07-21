de64bf7c19c2ba5c3254ae2ca3be4560d29c7484
Frank Stallone <frank@stallone.dev>
2026-07-20 20:45:12 -0400

    feat: illustrate preference versus judgment

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,9 +1,11 @@
 import Highlight from './Highlight.astro'
 import RailDigression from './RailDigression.astro'
 import CraftWorkflowIllustration from './CraftWorkflowIllustration.astro'
+import TasteJudgmentIllustration from './TasteJudgmentIllustration.astro'
 
 export const mdxComponents = {
   CraftWorkflowIllustration,
   Highlight,
   RailDigression,
+  TasteJudgmentIllustration,
 }
